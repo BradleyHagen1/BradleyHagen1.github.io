bae91c47b68c852191c9f3014d0b4afa12871282
Bradley Hagen <Brad.hagen94@gmail.com>
2023-04-12 15:47:03 -0700

    added skills & projects section

--- portfolio/src/components/About.jsx
+++ portfolio/src/components/About.jsx
@@ -1,43 +1,44 @@
 import React from "react";
 
-
 export default function About() {
-    return (
-        <section id="about">
-            <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
-                <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
-                    <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
-                        Hello, I'm Bradley.
-                        <br className="hidden lg:inline-block"/> I am a full stack web developer.
-                    </h1>
-                    <p className="mb-8 leading-relaxed">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere veniam cum fugit ullam, maxime,
-                         excepturi sunt modi voluptate quo iusto perferendis magnam impedit, 
-                         reprehenderit non aperiam totam hic laborum eligendi!
-                    </p>
-                    <div className="flex justify-center">
-                        <a 
-                        href="#contact"
-                        className="inline-flex text-white bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-green-600 rounded text-lg"
-                        >
-                            Contact Me!
-                        </a>
-                        <a
-                        href="#projects"
-                        className="ml-4 inline-flex text-white bg-gray-800 border-0 py-2 px-6 focus:outline-none hover:bg-gray-700 hover:text-white rounded text-lg"
-                        >
-                            My Past Work
-                        </a>
-                    </div>
-                </div>
-                  <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
-                    <img
-                    className="object-cover object-center rounded"
-                    alt="computer"
-                    src="https://images.unsplash.com/photo-1484417894907-623942c8ee29?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1632&q=80"
-                    />
-              </div>
-            </div>
-        </section>
-    )
+  return (
+    <section id="about">
+      <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
+        <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
+          <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
+            Hello, I'm Bradley.
+            <br className="hidden lg:inline-block" /> I am a full stack web
+            developer.
+          </h1>
+          <p className="mb-8 leading-relaxed">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere
+            veniam cum fugit ullam, maxime, excepturi sunt modi voluptate quo
+            iusto perferendis magnam impedit, reprehenderit non aperiam totam
+            hic laborum eligendi!
+          </p>
+          <div className="flex justify-center">
+            <a
+              href="#contact"
+              className="inline-flex text-white bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-green-600 rounded text-lg"
+            >
+              Contact Me!
+            </a>
+            <a
+              href="#projects"
+              className="ml-4 inline-flex text-white bg-gray-800 border-0 py-2 px-6 focus:outline-none hover:bg-gray-700 hover:text-white rounded text-lg"
+            >
+              My Past Work
+            </a>
+          </div>
+        </div>
+        <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
+          <img
+            className="object-cover object-center rounded"
+            alt="computer"
+            src="https://images.unsplash.com/photo-1484417894907-623942c8ee29?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1632&q=80"
+          />
+        </div>
+      </div>
+    </section>
+  );
 }
---
+++ portfolio/src/data.js
@@ -0,0 +1,63 @@
+export const projects = [
+  {
+    title: "A Cooks Book",
+    // subtitle: "x-stack",
+    description: "A site for hosting your favorite recipes",
+    image:
+      "https://images.unsplash.com/photo-1588960952097-4cf35f0a0306?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80",
+    link: "https://github.com/BradleyHagen1/specs-capstone.git",
+  },
+  {
+    title: "Activity Duck",
+    // subtitle:"",
+    description: "A site used to find and add fun activities to do",
+    image:
+      "https://images.unsplash.com/photo-1521336575822-6da63fb45455?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
+    link: "https://github.com/BradleyHagen1/capstone.git",
+  },
+];
+
+export const skills = [
+  {
+    title: "JavaScript",
+    description:
+      "A high level programming language used to build web applications",
+    image: "https://cdn-icons-png.flaticon.com/512/5968/5968292.png",
+  },
+  {
+    title: "React",
+    description: "A JavaScript library used to build user interfaces",
+    image:
+      "https://cdn.iconscout.com/icon/free/png-256/react-1-282599.png?f=webp&w=256",
+  },
+  {
+    title: "Redux",
+    description: "",
+    image: "",
+  },
+  {
+    title: "CSS",
+    description: "",
+    image: "",
+  },
+  {
+    title: "HTML",
+    description: "",
+    image: "",
+  },
+  {
+    title: "PostgreSQL",
+    description: "",
+    image: "",
+  },
+  {
+    title: "Node",
+    description: "",
+    image: "",
+  },
+  {
+    title: "Git",
+    description: "",
+    image: "",
+  },
+];
